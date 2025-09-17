ce7a5f9d8235e076f60704fd64a2371791c6deb2
Priscila Rosales <priscilamrosalesp@gmail.com>
2025-09-17 13:07:23 -0600

    chore(js): comprobando enlace de app.js y DOMContentLoaded

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+console.log("✅ app.js cargado");
+
+const $ = (s) => document.querySelector(s);
+const statusBox = $("#status");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("✅ DOM listo");
+  if (statusBox) {
+    statusBox.className = "notification is-info";
+    statusBox.textContent = "JS conectado correctamente ✅";
+  }
+});
 
